753e93941e9289797b1ceab577e10f226e7bed19
Erin Szabo <eszabo@cocc.edu>
2022-03-17 19:43:12 -0700

    Test Die attributes and methods except roll.

--- spec/die_spec.rb
+++ spec/die_spec.rb
@@ -2,6 +2,8 @@ require_relative '../lib/die'
 
 describe 'A die' do
 
+  #let(:die){ build(:die) }
+
   describe 'attributes' do
     it 'has a number of sides' do
       die = Die.new
@@ -16,23 +18,27 @@ describe 'A die' do
 
   describe 'number of sides' do
     it 'has six sides by default' do
-      skip
+      die = Die.new
+      expect(die.number_of_sides).to eq(6)
     end
 
     it 'can be created with a custom number of sides' do
-      skip
+      die = Die.new(33)
+      expect(die.number_of_sides).to eq(33)
     end
   end
 
   describe 'face value' do
     it 'has a face value of 1 by default' do
-      skip
+      die = Die.new
+      expect(die.face_value).to eq(1)
     end
   end
 
   describe '#to_s string representation' do
-    it 'TODO' do
-      skip
+    it 'displats the face_value of the die' do
+      die = Die.new
+      expect(die.to_s).to eq("1")
     end
   end
 
---
+++ spec/factories/dies.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :die do
+    #
+  end
+end
